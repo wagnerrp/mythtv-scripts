a4e25929712d8148737b75e5a3f50272643d0e1d
Raymond Wagner <rwagner@mythtv.org>
2012-04-16 13:51:09 -0400

    Add ability to correct for bad files from broken hash command.

--- python/hash_videos.py
+++ python/hash_videos.py
@@ -11,14 +11,17 @@
 
 from MythTV import Video
 
+QUIET = True
+
 def print_aligned(left, right):
     indent = 100 - len(left)
     print left, indent*' ', right
 
 for vid in Video.getAllEntries():
-    if vid.hash != 'NULL':
+    if vid.hash in ('NULL', '', 'UNKNOWN_COMMAND'):
+        vid.hash = vid.getHash()
+        vid.update()
+        print_aligned(vid.filename, vid.hash)
+    elif not QUIET:
         print_aligned(vid.filename, 'skipped')
-        continue
-    vid.hash = vid.getHash()
-    vid.update()
-    print_aligned(vid.filename, vid.hash)
+
